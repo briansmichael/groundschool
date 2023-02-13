a6804693e201c0fac44248b9f8bede618f5097db
Brian Michael <brian@cajunbug.net>
2023-02-13 11:07:59 -0500

    Initial checkin

---
+++ src/main/java/com/starfireaviation/groundschool/GroundschoolApplication.java
@@ -0,0 +1,13 @@
+package com.starfireaviation.groundschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroundschoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroundschoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/starfireaviation/groundschool/GroundschoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starfireaviation.groundschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroundschoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
