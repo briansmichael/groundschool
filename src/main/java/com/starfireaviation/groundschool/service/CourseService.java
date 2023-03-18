dc949abfd6db9def3f25004b288833a480fbbc6b
Brian Michael <brian@cajunbug.net>
2023-03-18 09:40:45 -0400

    Enhancing update process

--- src/main/java/com/starfireaviation/groundschool/service/CourseService.java
+++ src/main/java/com/starfireaviation/groundschool/service/CourseService.java
@@ -9,8 +9,7 @@ import java.util.List;
 @Service
 public class CourseService {
 
-    //public static final String COMPLETE_COURSE_LIST = "AMA,AMG,AMP,ATP,CFI,COM,FLE,IFR,IOF,MCI,MIL,PAR,PVT,SPG,SPI"; //RDP
-    public static final String COMPLETE_COURSE_LIST = "PVT,SPG,COM";
+    public static final String COMPLETE_COURSE_LIST = "AMA,AMG,AMP,ATP,CFI,COM,FLE,IFR,IOF,MCI,MIL,PAR,PVT,SPG,SPI"; //RDP
 
     public List<String> getCourseList() {
         return new ArrayList<>(Arrays.stream(COMPLETE_COURSE_LIST.split(",")).toList());
